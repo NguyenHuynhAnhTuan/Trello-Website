92bdbb13a8ca96b502480eb5100e3face983a679
Nguyen Huynh Anh Tuan <nguyenhuynhanhtuan2407@gmail.com>
2025-03-11 11:00:01 +0700

    Trello Web First Step

--- trello-website-frontend/src/App.jsx
+++ trello-website-frontend/src/App.jsx
@@ -1,20 +1,87 @@
-import Delete from '@mui/icons-material/Delete'
-import AddToHomeScreenOutlined from '@mui/icons-material/AddToHomeScreenOutlined'
-import AdbOutlined from '@mui/icons-material/AdbOutlined'
-import Button from '@mui/material/Button'
+//Import Icon
+import LightMode from '@mui/icons-material/LightMode'
+import DarkMode from '@mui/icons-material/DarkMode'
+import SettingsBrightness from '@mui/icons-material/SettingsBrightness'
+
+//Import Component
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material/Box'
+
+import { useColorScheme } from '@mui/material/styles'
+import Container from '@mui/material/Container'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-mode"
+        id="select-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value={'light'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
+            <LightMode fontSize='small'></LightMode> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
+            <DarkMode fontSize='small'></DarkMode> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
+            <SettingsBrightness fontSize='small'></SettingsBrightness> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 function App() {
   return (
-    <>
-      <div>Tuan Nguyen Developer</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <div>Im learning ReactJS</div>
-      <AdbOutlined></AdbOutlined>
-      <AddToHomeScreenOutlined></AddToHomeScreenOutlined>
-      <Delete></Delete>
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect></ModeSelect>
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Content
+      </Box>
+    </Container>
   )
 }
 
